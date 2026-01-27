dc714ff3837ea49e7c9a4a698f408dc9a2540a9a
Seongbin Yoon <gsc2423020@g.yju.ac.kr>
2026-01-27 12:56:23 +0900

    feat: function 3 declarations

---
+++ test/test11.js
@@ -0,0 +1,14 @@
+// Function 3 declarations
+
+console.log(add1(2, 3));
+console.log(add2(4, 5));
+console.log(add3(5, 6));
+
+// Literal function
+function add1(x, y) { return x + y; }
+
+// Function express (Anonymous function)
+let add2 = function () { return x + y; }
+
+// Arrow function (ES6, ECMA 2015)
+let add3 = () => { return x + y; }
