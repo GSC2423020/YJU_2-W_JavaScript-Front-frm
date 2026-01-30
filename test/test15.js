7ad76033cf31d151566399771b5b8f2af968c200
Seongbin Yoon <gsc2423020@g.yju.ac.kr>
2026-01-30 14:22:27 +0900

    feat: JavaScript inheritance

---
+++ test/test15.js
@@ -0,0 +1,24 @@
+class Person {
+    name = undefined; age = undefined;
+    constructor(argName, argAge) {
+        this.name = argName;
+        this.age = argAge;
+    }
+
+    prtInfo() {
+        console.log(`name: ${this.name}, age"${this.age}`);
+    }
+}
+
+class Student extends Person {
+    dept = undefined;
+    constructor(argName, argAge, argDept) {
+        super(argName, argAget);
+        this.dept = argDept;
+    }
+
+    prtInfo() {
+        super.prtInfo();
+        console.lof(`dept: ${this.dept}`);
+    }
+}
