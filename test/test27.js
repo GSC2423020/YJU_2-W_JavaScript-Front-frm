8a5edf401f6753666ac7a037cee24775b5d4f949
Seongbin Yoon <gsc2423020@g.yju.ac.kr>
2026-02-13 12:35:26 +0900

    feat: add Promise rejection and error propagation example

---
+++ test/test27.js
@@ -0,0 +1,15 @@
+new Promise((resolve, reject) => {
+    // resolve("Executor: S");
+    reject("Executor: F");
+}).then(
+    (result) => {
+        console.log(`then-S: ${result}`);
+    },
+    (error) => {
+        console.log(`then-F: ${error}`);
+        return Promise.reject("reject-Then");
+    },
+)
+    // Exception Handling
+    .catch((error) => console.log(`catch: ${error}`))
+    .finally((result) => console.log(`finally: ${result}`));
